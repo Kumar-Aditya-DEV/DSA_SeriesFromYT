434576aea25d37473523b9fca8f7494f0b7e6f8c
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-02-13 05:50:47 +0530

    Optimize power of 2 implementation

--- Lecture7_LeetCode_Problem_Solving_Session/03PowerOfTwo231.cpp
+++ Lecture7_LeetCode_Problem_Solving_Session/03PowerOfTwo231.cpp
@@ -1,5 +1,29 @@
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout << "Enter Your number: ";
+//     cin >> n;
+
+//     for(int i = 0;i <= 30 ; i++){
+//       int ans = pow(2,i);
+//       if(ans == n){
+//         return true;
+//       }
+//     }
+//     return false;
+//     return 0;
+// }
+
+
+
+
+
+
 #include<iostream>
-#include<math.h>
+#include<climits>
 using namespace std;
 
 int main(){
@@ -7,12 +31,19 @@ int main(){
     cout << "Enter Your number: ";
     cin >> n;
 
-    for(int i = 0;i <= 30 ; i++){
-      int ans = pow(2,i);
-      if(ans == n){
-        return true;
-      }
+    int ans = 1;
+
+    for(int i = 0; i <= 30; i++){
+        if(ans == n){
+            cout << "True";
+            return 0;
+        }
+
+        if(ans < INT_MAX / 2){
+            ans = ans * 2;
+        }
     }
-    return false;
+
+    cout << "False";
     return 0;
 }
