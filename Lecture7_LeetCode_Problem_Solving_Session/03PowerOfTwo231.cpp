e9eff85142cc97a25a64e70c848fd9c712af26d2
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-02-13 05:40:58 +0530

    Implement power of 2 check using loop and pow()

---
+++ Lecture7_LeetCode_Problem_Solving_Session/03PowerOfTwo231.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter Your number: ";
+    cin >> n;
+
+    for(int i = 0;i <= 30 ; i++){
+      int ans = pow(2,i);
+      if(ans == n){
+        return true;
+      }
+    }
+    return false;
+    return 0;
+}
