3af9c5e2a163a0f4af76ec2db33a7bd24a67ced0
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-07 12:05:59 +0530

    learn datatypes

---
+++ Apna_College/02Variable_DataTypes_&_Operators/02DataTypes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int age = 25;
+  char grade = 'a';
+  float PI = 3.14f;
+  bool isSafe = true;
+  cout << age << endl;
+  cout << sizeof(age) << endl;
+  return 0; 
+}
+
+
+// Boilerplate Code - Comment
