cf5ff69f2e246fad88121b7270f2c74808e895b4
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-07 12:07:31 +0530

    learn how to take input in cpp

---
+++ Apna_College/02Variable_DataTypes_&_Operators/04TakeInput.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int price;
+  cout << "Enter the age : ";
+  cin >> price;
+
+  cout << "You entered price = " << price << endl;
+  return 0; 
+}
