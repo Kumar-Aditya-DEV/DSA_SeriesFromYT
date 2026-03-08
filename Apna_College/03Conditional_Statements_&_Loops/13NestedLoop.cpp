95bd406cb136307c7a737942ccf6fdaa01f3fe42
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-08 20:31:41 +0530

    learn nestedLoop and its use

---
+++ Apna_College/03Conditional_Statements_&_Loops/13NestedLoop.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  // for(int i=1; i<=5; i++){
+  //   cout << "*****" << endl;
+  // }
+
+  // int x = 10;
+  // for(int i=1; i<=x; i++){
+  //   cout << "*";
+  // }
+  // cout << endl;
+
+
+  int n = 5;
+  for(int i=1; i<=5; i ++){
+    int m = 5;
+    for(int j=1; j<=m; j++){
+      cout << "*";
+    }
+    cout << endl;
+  }
+  return 0;
+}
