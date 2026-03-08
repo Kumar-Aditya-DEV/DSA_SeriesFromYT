fcd599784d47ed136657675f4badc04bc9e9f11b
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-08 20:25:52 +0530

    learn while loop and solve the qusn.

---
+++ Apna_College/03Conditional_Statements_&_Loops/07While_Loops.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // int count = 1;
+  // while (count <= 5)
+  // {
+  //   cout << count << " ";
+  //   count++;
+  // }
+  
+
+  int n = 20;
+  int count = 1;
+  while (count <= n)
+  {
+    cout << count << " ";
+    count++;
+  }
+  
+  cout << endl;
+  return 0;
+} 
