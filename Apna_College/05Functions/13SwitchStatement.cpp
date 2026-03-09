62ecf95069567c5344e7457470f931d976c603f2
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-09 23:58:03 +0530

    learn switch statement

---
+++ Apna_College/05Functions/13SwitchStatement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main(){
+
+
+  // boiler plate
+// switch(variable) {
+
+//     case value1:
+//         // code
+//         break;
+
+//     case value2:
+//         // code
+//         break;
+
+//     default:
+//         // code
+// }
+
+}
