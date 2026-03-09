907a516b21bab7afc7aad94db98aa89ebd64e0eb
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-09 23:46:21 +0530

    complete inverted triangle pattern with char

---
+++ Apna_College/04Patterns/14InvertedTrianglePatternChar.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+    
+    for(int i = 0; i < n; i++) {
+      // spaces
+        for(int j = 0; j < i ; j++) {
+            cout << " ";
+        }
+
+        // nums
+        for(int k=0;k<n-i;k++){
+          cout << char('A' + i);
+        }
+        cout << endl;
+    }
+
+    return 0;
+} 
