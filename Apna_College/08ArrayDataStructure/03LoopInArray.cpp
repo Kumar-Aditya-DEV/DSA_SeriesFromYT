8ddf2445abad6cba70d2c325f72ead526d8f7c4d
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-10 22:56:08 +0530

    learn loop in array

---
+++ Apna_College/08ArrayDataStructure/03LoopInArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // int marks[5] = {11, 123, 44, 55, 64};
+  // int size = 5;
+
+  // for(int i=0; i<size; i++){
+  //   cout << marks[i] << endl;
+  // }
+
+
+
+    // total array ka size
+  // int size = sizeof(marks) / sizeof(int);
+  // cout << size << endl;
+
+  
+  return 0;
+}
