3edf034c4bf59a6d5f6f7a248a20105c3303ac53
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-10 23:08:51 +0530

    learn local scope variable

---
+++ Apna_College/07BitwiseOperator_DataType/08LocalScopeVariable.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//   if(3 > 1){
+//   }
+//   else{
+//     int x = 10;
+//   }
+
+// cout << x << endl;
+
+
+
+// for(int i=0; i<10; i++){
+// }
+
+// cout << i << endl;
+
+
+
+// {
+//   int x = 10;
+// }
+
+// cout << x << endl;
+
+
+
+
+return 0;
+}
+
