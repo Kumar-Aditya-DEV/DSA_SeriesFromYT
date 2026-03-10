0ed9c4b9b66caaf728d9250e9d008b88c7bcb912
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-10 22:55:29 +0530

    learn how to access the array

---
+++ Apna_College/08ArrayDataStructure/02IndexArray.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int marks[5] = {11, 123, 44, 55, 64};
+
+  cout << marks[0] << endl;
+  cout << marks[1] << endl;
+  cout << marks[2] << endl;
+  cout << marks[3] << endl;
+  cout << marks[4] << endl;
+
+  return 0;
+}
