2187e90b282a5d5123e9de384268ec0ed67f6f85
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-12 22:43:57 +0530

    modify the code

--- Apna_College/08ArrayDataStructure/04TakingInputArray.cpp
+++ Apna_College/08ArrayDataStructure/04TakingInputArray.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main(){
 
+  // method 1 -> when size is already given 
+   
   // size taking input in array
   int size = 5;
   int marks[size];
@@ -15,6 +17,24 @@ int main(){
   for(int i=0; i<size; i++){
     cout << marks[i] << endl;
   }
+
+
+
+
+
+  // method 2 -> take size and marks both as input
+
+    int size;
+    cin >> size;
+    
+    int marks[size];
+    for(int i=0; i<size; i++){
+        cin >> marks[i];
+    }
+    
+    for(int i=0; i<size; i++){
+        cout << marks[i] << endl;
+    }
   
   return 0;
 }
