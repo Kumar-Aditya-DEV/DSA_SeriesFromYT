89d96f964d965f4de073078c936d7e57d998f998
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-12 22:42:57 +0530

    add some data

--- Apna_College/08ArrayDataStructure/01BasicsArray.cpp
+++ Apna_College/08ArrayDataStructure/01BasicsArray.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
+  int markss[5]; 
+
   int marks[5] = {11, 123, 44, 55, 64};
 
-  double price[] = {20.22, 33.3, 44.3, 30.00};  // 3
+  double price[] = {20.22, 33.3, 44.3, 30.00};  // 4 
 
   return 0;
 }
