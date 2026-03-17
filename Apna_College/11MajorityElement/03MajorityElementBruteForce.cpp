24602a319346a5da5d2e08898b8ea475e9c4e4b9
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-17 22:23:57 +0530

    solve majority element with brute force method

---
+++ Apna_College/11MajorityElement/03MajorityElementBruteForce.cpp
@@ -0,0 +1,59 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var majorityElement = function(nums) {
+//   int n = nums.size();
+//     for(int val : nums){
+//       int freq = 0;
+
+//       for(int el : nums){
+//         if(el == val){
+//           freq++;
+//         }
+//       }
+//       if(freq > n/2){
+//         return val;
+//       }
+//     }
+//     return -1;
+// };
+
+
+
+
+
+
+
+
+// run this in vs code 
+#include <bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+    int n = nums.size();
+
+    for (int val : nums) {
+        int freq = 0;
+
+        for (int el : nums) {
+            if (el == val) {
+                freq++;
+            }
+        }
+
+        if (freq > n / 2) {
+            return val;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
+
+    cout << majorityElement(nums);
+
+    return 0;
+}
