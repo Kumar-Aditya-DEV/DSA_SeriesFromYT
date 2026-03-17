27e1c525c5ecc90be106e0fa9464001689b7f58a
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-17 22:25:11 +0530

    solve majority element with moores voting algorithm

---
+++ Apna_College/11MajorityElement/05MooresVotingAlgorithm.cpp
@@ -0,0 +1,55 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var majorityElement = function(nums) {
+//   int freq = 0; ans = 0;
+
+//   for(int i=0; i<nums.size(); i++){
+//     if(freq = 0){
+//       ans = nums[i];
+//     }
+//     if(ans == nums[i]){
+//       freq++;
+//     }
+//     else{
+//       freq--;
+//     }
+//   }
+// return ans;
+// }; 
+
+
+
+
+
+
+
+// optimise approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+    int freq = 0;
+    int ans = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (freq == 0) {
+            ans = nums[i];
+        }
+
+        if (ans == nums[i]) {
+            freq++;
+        } else {
+            freq--;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {2,2,1,1,1,2,2};
+    cout << majorityElement(nums);
+    return 0;
+}
