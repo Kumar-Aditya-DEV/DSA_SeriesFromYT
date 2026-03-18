4f739837577cead616aa2beab6bca15796b5e815
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-18 13:48:17 +0530

    add comments in code

--- Apna_College/06BinaryNumberSystem.cpp/01DecimalToBinary.cpp
+++ Apna_College/06BinaryNumberSystem.cpp/01DecimalToBinary.cpp
@@ -1,29 +1,36 @@
 #include<iostream>
 using namespace std;
+
+// Function to convert Decimal to Binary
 int decToBinary(int decNum) {
-int ans = 0, pow = 1;
+    
+    int ans = 0;   // final binary number store karega
+    int pow = 1;   // place value (1, 10, 100, ...)
 
-while(decNum > 0) {
-int rem = decNum % 2;
-decNum /= 2;
+    // jab tak decimal number 0 nahi hota
+    while(decNum > 0) {
+        
+        int rem = decNum % 2;   // remainder (0 ya 1) → binary digit
+        decNum /= 2;            // number ko 2 se divide kar rahe
 
-ans += (rem * pow);
-pow *= 10;
+        ans += (rem * pow);     // remainder ko correct position pe add karna
+        pow *= 10;              // next position (10, 100, 1000...)
+    }
 
+    return ans; // binary form return karega (integer format me)
 }
-return ans; //binary form
-}
-
 
 int main() {
-int decNum = 50;
 
-cout << decToBinary(decNum) << endl;
+    int decNum = 50;
 
+    // 50 ka binary print karega
+    cout << decToBinary(decNum) << endl;
 
-// binary from 1 to 10
-for(int i=1; i<=10; i++){
-  cout << decToBinary(i) << endl;
-}
-return 0;
+    // 1 se 10 tak sabka binary print karega
+    for(int i = 1; i <= 10; i++){
+        cout << decToBinary(i) << endl;
+    }
+
+    return 0;
 }
