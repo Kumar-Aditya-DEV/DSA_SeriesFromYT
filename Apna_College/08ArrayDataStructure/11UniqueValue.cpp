42c8206a80f122398ec15a08691eb4f04216e426
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-18 15:14:20 +0530

    correct our code and add comments

--- Apna_College/08ArrayDataStructure/11UniqueValue.cpp
+++ Apna_College/08ArrayDataStructure/11UniqueValue.cpp
@@ -1,15 +1,22 @@
 #include<iostream>
 using namespace std;
 
+// Function unique elements print karega
 void printUnique(int arr[], int size){
+
   for(int i=0; i < size; i++){
-    int count = 0;
-    for(int j=0; j < size; i++){
+    
+    int count = 0;  // count karega kitni baar element aaya
+
+    // har element ko sabse compare karo
+    for(int j=0; j < size; j++){   // ✅ j++ correct hai
+      
       if(arr[i] == arr[j]){
-        count++;
+        count++;   // match mila to count badhao
       }
     }
 
+    // agar sirf 1 baar aaya → unique hai
     if(count == 1){
       cout << arr[i] << " ";
     }
@@ -17,9 +24,11 @@ void printUnique(int arr[], int size){
 }
 
 int main(){
+
   int arr[] = {1,2,2,3,4,4,5};
   int size = 7;
 
   printUnique(arr, size);
+
   return 0;
 }
