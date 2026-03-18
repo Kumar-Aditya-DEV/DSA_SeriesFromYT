8ddb1963402b105a5ec16e2db82fe825c78f58be
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-18 14:15:56 +0530

    modifiy and add comments

--- Apna_College/07BitwiseOperator_DataType/12ReverseAnumber.cpp
+++ Apna_College/07BitwiseOperator_DataType/12ReverseAnumber.cpp
@@ -1,21 +1,25 @@
 #include<iostream>
 using namespace std;
 
+// Function number ko reverse karega
 int reverseNumber(int n){
-    int rev = 0;
+    
+    int rev = 0;  // reversed number store karega
 
+    // jab tak number 0 nahi ho jata
     while(n != 0){
-        int digit = n % 10;
-        rev = rev * 10 + digit;
-        n = n / 10;
+        
+        int digit = n % 10;        // last digit nikalte hain
+        rev = rev * 10 + digit;    // reverse me add karte hain
+        n = n / 10;                // last digit remove karte hain
     }
 
-    return rev;
+    return rev; // final reversed number
 }
 
 int main(){
     int n;
-    cin >> n;
+    cin >> n;  // user input
 
-    cout << reverseNumber(n);
+    cout << reverseNumber(n); // reversed number print
 }
