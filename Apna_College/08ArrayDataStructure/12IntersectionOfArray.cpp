6d5da95d24ddfede17558db80e1ad531221225a5
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-18 15:22:56 +0530

    correct our code and add comments

--- Apna_College/08ArrayDataStructure/12IntersectionOfArray.cpp
+++ Apna_College/08ArrayDataStructure/12IntersectionOfArray.cpp
@@ -1,20 +1,21 @@
 #include<iostream>
 using namespace std;
 
+// Function common elements print karega (arr1 ∩ arr2)
 void printUnique(int arr1[], int size1, int arr2[], int size2){
-  for(int i=0; i < size1; i++){
-  
 
+  for(int i=0; i < size1; i++){
+    
+    // har element arr1 ka arr2 me check karo
     for(int j=0; j < size2; j++){
+      
       if(arr1[i] == arr2[j]){
-        cout << arr1[i] << " ";
-        break;
+        cout << arr1[i] << " ";  // match mila → print
+        break;  // ek baar mil gaya to aage check nahi karna
+      }
     }
   }
 }
-}
-
-
 
 int main(){
   int arr1[] = {1,2,2,3,4,4,5};
