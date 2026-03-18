c032b7b1b50f695dc163d121161890868ce23eb2
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-18 14:37:20 +0530

    modifiy the code  and add comments

--- Apna_College/08ArrayDataStructure/05SmallestAndLargestInArray.cpp
+++ Apna_College/08ArrayDataStructure/05SmallestAndLargestInArray.cpp
@@ -1,47 +1,65 @@
 // ye code mere ko value nikl kr dega smallest ya largest 
 
-// #include<iostream>
-// #include<climits>
-// using namespace std;
-// int main(){
-//   // int nums[] = {10, 20, -10, 30, 45, 101};
-//   // int size = 6;
+#include<iostream>
+#include<climits>
+using namespace std;
 
-//   // int smallest = INT_MAX;
+int main(){
 
-//   // for(int i=0; i<size; i++){
+  int nums[] = {10, 20, -10, 30, 45, 101};
+  int size = 6;
 
-//       //  normal method
-//   //   if(nums[i] < smallest){
-//   //     smallest = nums[i];
-//   //   }
+  int largest = INT_MIN;  // sabse chhoti possible value se start
 
-//         // inbuilt method direct
-//         // smallest = min(nums[i], smallest);
-//   // }
+  for(int i=0; i<size; i++){
 
-//   // cout << "smallest = " << smallest << endl;
+    // agar current element bada hai to update karo
+    // largest = max(nums[i], largest); // inbuilt method
 
+    if(nums[i] > largest){
+      largest = nums[i];
+    }
+  }
 
+  cout << "largest = " << largest << endl;
 
-//   int nums[] = {10, 20, -10, 30, 45, 101};
-//   int size = 6;
-
-//   int largest = INT_MIN;
-  
-//   for(int i=0; i<size; i++){
-//     // if(nums[i] > largest){
-//     //   largest = nums[i];
-//     // }
+  return 0;
+}
 
 
-//     // inbuilt direct
-//     largest = max(nums[i], largest);
-//   }
 
-//   cout << "largest  = "   << largest << endl;
-//   return 0;
-// } 
+
+
+
+// for largest
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{    
+    int nums[] = { 2, 4, 5, 8, -10, 20};
+    int size = 6;
+    
+    int largest = INT_MIN; // starting me smallest possible value
+    int index = -1;        // index store karega
+    
+    for(int i=0; i<size; i++){
+        
+        // agar bada element mila
+        if(nums[i] > largest){
+            largest = nums[i]; // value update
+            index = i;         // index update
+        }
+    }
+    
+    cout << "Largest = " << largest << endl;
+    cout << "Index = " << index << endl;
+
+    return 0;
+}
+
 
 
 
@@ -52,8 +70,6 @@
 
 
 // ye mere ko index ki value dega 
-
-
 
 
 #include <iostream>
