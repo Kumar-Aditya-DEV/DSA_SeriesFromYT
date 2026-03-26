849414a6cc76c640490b684593c978df176a1faa
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-26 19:13:39 +0530

    learn null pointer

---
+++ Apna_College/16Pointers/04NullPointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+  int** ptr;        // isko cout kariyenge to hame garbage address dega 
+
+
+  int** ptr = NULL;      // gives zero in output 
+  cout <<  ptr << endl;
+
+  
+
+         
+  return 0;
+}
