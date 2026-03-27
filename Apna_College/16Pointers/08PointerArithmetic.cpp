218c7731c86083fc4c552c2457bc663363231d53
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-27 22:58:22 +0530

    add comments in code

--- Apna_College/16Pointers/08PointerArithmetic.cpp
+++ Apna_College/16Pointers/08PointerArithmetic.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main(){
 
-  // Incriment and decriment
+  // ---------------- INCREMENT / DECREMENT ----------------
   int a = 10;
   int *ptr = &a;
 
   cout << ptr << endl;
+  // ptr → a ka address
+
   ptr++;
-  cout << ptr << endl;      // +4 
+  // ptr next memory location pe move karega
+  // 👉 int size = 4 bytes → address +4 hoga
+
+  cout << ptr << endl;      
+  // previous address + 4
+
   cout << endl; 
 
 
 
-  // add / Subtract Number
+  // ---------------- ADD / SUBTRACT ----------------
   int b = 10;
   int *ptr2 = &b;
 
   cout << ptr2 << endl;
+  // ptr2 → b ka address
+
   ptr2 = ptr2 + 2;
-  cout << ptr << endl;      // +8
+  // ptr2 2 positions aage move karega
+  // 👉 2 * sizeof(int) = 2 * 4 = +8 bytes
+
+  cout << ptr2 << endl;      
+  // previous address + 8
+
   return 0;
 }
