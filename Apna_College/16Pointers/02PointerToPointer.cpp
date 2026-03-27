f1a7a4b8d121800e5e4e15151faae167afecb424
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-27 22:55:15 +0530

    modify code and add comments in code

--- Apna_College/16Pointers/02PointerToPointer.cpp
+++ Apna_College/16Pointers/02PointerToPointer.cpp
@@ -4,14 +4,43 @@ using namespace std;
 
 int main(){
 
-  int b = 10;
+  int b = 10;  
+  // normal variable b jisme value 10 hai
+
   int* ptr = &b;   
-  
-  int** parentPtr = &ptr;      // agar mere ko pointer ka address store karana hai tb mai ye likhunga
+  // ptr ek pointer hai jo b ka address store karta hai
+
+  int** parentPtr = &ptr;      
+  // parentPtr ek pointer hai jo ptr ka address store karta hai
+  // 👉 yani pointer ka pointer
 
   cout << ptr << endl;              
+  // ptr → b ka address print karega
+
   cout << &b << endl;             
-  cout << &ptr << endl;          // same value 
-  cout << parentPtr << endl;    // same value    
+  // &b → b ka address print karega
+  // 👉 ptr aur &b same honge
+
+  cout << &ptr << endl;          
+  // &ptr → ptr ka address print karega
+
+  cout << parentPtr << endl;    
+  // parentPtr → ptr ka address store karta hai
+  // 👉 isliye parentPtr aur &ptr same honge
+
   return 0;
 }
+
+
+
+
+
+
+
+// 🔥 Easy Trick yaad rakh
+// Expression	Meaning
+// p	          address of a
+// *p	        value of a
+// pp	        address of p
+// *pp	        p
+// **pp	      value of a
