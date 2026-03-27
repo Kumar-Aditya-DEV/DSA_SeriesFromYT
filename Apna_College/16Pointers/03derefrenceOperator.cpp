73b49ee9906ed6aa193d0baf6976bfdfb179f55d
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-27 22:55:52 +0530

    modify code and for revision add comments in code

--- Apna_College/16Pointers/03derefrenceOperator.cpp
+++ Apna_College/16Pointers/03derefrenceOperator.cpp
@@ -5,24 +5,42 @@ using namespace std;
 int main(){
 
   int a = 10;
+  // normal variable a = 10
+
   int* ptr = &a;
+  // ptr me a ka address store hai
 
   int** parePtr = &ptr;
+  // parePtr me ptr ka address store hai (pointer to pointer)
 
-  cout <<  (ptr) << endl;             // same memory address
-  cout << *(parePtr) << endl;         // same memory addres
-  cout << **(parePtr) << endl;        // double derefrence ka output -> 10
- 
+  cout << (ptr) << endl;             
+  // ptr → a ka address print karega
+
+  cout << *(parePtr) << endl;         
+  // *parePtr → ptr ki value
+  // ptr ki value = a ka address
+  // 👉 to ye bhi a ka address print karega
+
+  cout << **(parePtr) << endl;        
+  // **parePtr → a ki actual value
+  // 👉 output = 10
 
 
   int b = 10;
-  int* ptr1 = &b;        
-             
-  cout << *(&b) << endl;      //     uss address pr jo store value hai usko dega as an output    
-  cout << *(ptr1) << endl;      //     same output  
-  
-  
+  // new variable b
 
-         
+  int* ptr1 = &b;        
+  // ptr1 me b ka address
+
+  cout << *(&b) << endl;      
+  // &b → b ka address
+  // *(&b) → us address pe jo value hai → b
+  // 👉 output = 10
+
+  cout << *(ptr1) << endl;      
+  // ptr1 → b ka address
+  // *ptr1 → b ki value
+  // 👉 output = 10
+
   return 0;
 }
