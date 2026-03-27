c371a9f0a2c036c11a55ffd2f185cedb2adda793
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-27 22:50:40 +0530

    complete a code that help in binary search with 2 optimise methods

---
+++ Apna_College/17BinarySearchAlgorithm/01SortedApproach.cpp
@@ -0,0 +1,67 @@
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int st = 0, end = nums.size() - 1;
+
+//         while(st <= end){
+//             int mid = (st + end) / 2;
+
+//             if(target > nums[mid]) {
+//                 st = mid + 1;
+//             }
+//             else if(target < nums[mid]) {
+//                 end = mid - 1;
+//             }
+//             else {
+//                 return mid;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+
+
+
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int> arr, int tar){       //Iterative
+  int st=0, end=arr.size() - 1;
+  while (st <= end){
+    // int mid = (st + end) / 2;        // niche wla optimise code hai 
+    int mid = st + (end - st) / 2;
+
+    if(tar > arr[mid]){
+      st = mid + 1;       //2nd half
+    }
+    else if(tar < arr[mid]){
+      st = mid - 1;      //1st half
+    }
+    else{
+      return mid;
+    }
+  }
+
+
+  return -1;
+}
+
+int main() {
+vector<int> arr1 = {-1, 0, 3, 4, 5, 9, 12}; //odd
+int tar1 = 12;
+
+cout << binarySearch(arr1, tar1) << endl;
+
+vector<int> arr2 = {-1, 0, 3, 5, 9, 12}; //even
+int tar2 = 0;
+
+cout << binarySearch(arr2, tar2) << endl; 
+
+return 0;
+
+}
