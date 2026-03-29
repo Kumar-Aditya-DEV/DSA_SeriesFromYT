9c9592760e25fe0e6af13411aeab365e1e562728
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-29 10:17:41 +0530

    solve rotated sorted array with binary search method

--- Apna_College/18RotatedSortedArray/leetCode33.cpp
+++ Apna_College/18RotatedSortedArray/leetCode33.cpp
@@ -53,49 +53,60 @@
 
 
 
-// leetcode solution 
+// // leetcode solution 
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int st=0,end=nums.size() - 1;
+
+//         while(st <= end){
+//             int mid = st + (end - st) / 2;
+//             if(nums[mid] == target){
+//                 return mid;
+//             }
+
+//             if(nums[st] <= nums[mid]){    // left sorted
+//                if(nums[st] <= target && target <= nums[mid]){
+//                 end = mid - 1;
+//                }
+//                else{
+//                 st = mid + 1;
+//                }
+//             }
+
+//             else{       // right sorted
+//               if(nums[mid] <= target && target <= nums[end]){
+//                 st = mid + 1;
+//               }
+//               else{
+//                 end = mid - 1;
+//               }
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
+
+
+
+
+
+
+
+
+// with linear search but very slow gives tle
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int st=0,end=nums.size() - 1;
-
-        while(st <= end){
-            int mid = st + (end - st) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }
-
-            if(nums[st] <= nums[mid]){    // left sorted
-               if(nums[st] <= target && target <= nums[mid]){
-                end = mid - 1;
-               }
-               else{
-                st = mid + 1;
-               }
-            }
-
-            else{       // right sorted
-              if(nums[mid] <= target && target <= nums[end]){
-                st = mid + 1;
-              }
-              else{
-                end = mid - 1;
-              }
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                return i;
             }
         }
-
         return -1;
     }
 };
-
-
-
-
-
-
-
-
-
 
 
 
