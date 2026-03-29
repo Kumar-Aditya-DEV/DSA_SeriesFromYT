709cfda6838c219d61ec9bd7f33c7aca3488d4da
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-29 10:16:40 +0530

    solve rotated sorted array in leetcode

--- Apna_College/18RotatedSortedArray/leetCode33.cpp
+++ Apna_College/18RotatedSortedArray/leetCode33.cpp
@@ -1,52 +1,100 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
-int search(vector<int>& nums, int target) {
-    int st = 0, end = nums.size() - 1;
+// int search(vector<int>& nums, int target) {
+//     int st = 0, end = nums.size() - 1;
 
-    while (st <= end) {
-        int mid = st + (end - st) / 2;
+//     while (st <= end) {
+//         int mid = st + (end - st) / 2;
 
-        if (nums[mid] == target) {
-            return mid;
-        }
+//         if (nums[mid] == target) {
+//             return mid;
+//         }
 
-        // Left sorted
-        if (nums[st] <= nums[mid]) {
-            if (nums[st] <= target && target <= nums[mid]) {
+//         // Left sorted
+//         if (nums[st] <= nums[mid]) {
+//             if (nums[st] <= target && target <= nums[mid]) {
+//                 end = mid - 1;
+//             } else {
+//                 st = mid + 1;
+//             }
+//         }
+//         // Right sorted
+//         else {
+//             if (nums[mid] <= target && target <= nums[end]) {
+//                 st = mid + 1;
+//             } else {
+//                 end = mid - 1;
+//             }
+//         }
+//     }
+
+//     return -1;
+// }
+
+// int main() {
+//     vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+//     int target = 0;
+
+//     int result = search(nums, target);
+
+//     if (result != -1) {
+//         cout << "Element found at index: " << result << endl;
+//     } else {
+//         cout << "Element not found" << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
+// leetcode solution 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int st=0,end=nums.size() - 1;
+
+        while(st <= end){
+            int mid = st + (end - st) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[st] <= nums[mid]){    // left sorted
+               if(nums[st] <= target && target <= nums[mid]){
                 end = mid - 1;
-            } else {
+               }
+               else{
                 st = mid + 1;
+               }
+            }
+
+            else{       // right sorted
+              if(nums[mid] <= target && target <= nums[end]){
+                st = mid + 1;
+              }
+              else{
+                end = mid - 1;
+              }
             }
         }
-        // Right sorted
-        else {
-            if (nums[mid] <= target && target <= nums[end]) {
-                st = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
+
+        return -1;
     }
+};
 
-    return -1;
-}
 
-int main() {
-    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
-    int target = 0;
 
-    int result = search(nums, target);
 
-    if (result != -1) {
-        cout << "Element found at index: " << result << endl;
-    } else {
-        cout << "Element not found" << endl;
-    }
 
-    return 0;
-}
+
+
+
 
 
 
