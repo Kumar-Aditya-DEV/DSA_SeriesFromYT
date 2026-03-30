ecdb0b1e93fa68dd4a6750c6a532b36362a4d6b7
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-30 05:47:07 +0530

    solved leetcode problem with bitwise operator

--- Apna_College/20SingleElementInSortedArray/leetcode540.cpp
+++ Apna_College/20SingleElementInSortedArray/leetcode540.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        // with linear search
-        int n = nums.size();
+        // // with linear search
+        // int n = nums.size();
         
-        for(int i = 0; i < n - 1; i += 2){
-        if(nums[i] != nums[i + 1]){
-            return nums[i];
-            }
+        // for(int i = 0; i < n - 1; i += 2){
+        // if(nums[i] != nums[i + 1]){
+        //     return nums[i];
+        //     }
+        // }
+        // return nums[n - 1];
+
+
+        // with bit wise operator 
+        int ans = 0;
+        
+        for(int num : nums){
+        ans ^= num;
         }
-        return nums[n - 1];
+        return ans;
     }
 };
