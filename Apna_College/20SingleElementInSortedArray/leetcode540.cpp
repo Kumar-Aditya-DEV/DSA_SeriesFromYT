fa0f5e1c87062e502862b39d8757c6028a1a7658
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-30 05:50:16 +0530

    solved leetcode problem with optimise but long lines of code

--- Apna_College/20SingleElementInSortedArray/leetcode540.cpp
+++ Apna_College/20SingleElementInSortedArray/leetcode540.cpp
@@ -24,25 +24,62 @@ public:
 
 
 
-        // more optimise and short code
+        // // more optimise and short code
+        // int n = nums.size();
+
+        // if(n == 1) return nums[0];
+
+        // int st = 0, end = n - 1;
+
+        // while(st < end){
+        //     int mid = st + (end - st) / 2;
+
+        //     // make mid even
+        //     if(mid % 2 == 1) mid--;
+        //     if(nums[mid] == nums[mid + 1]){
+        //         st = mid + 2;
+        //     } else {
+        //         end = mid;
+        //     }
+        // }
+        // return nums[st];
+
+
+
+
+        
+        // optimise but long code
         int n = nums.size();
 
         if(n == 1) return nums[0];
 
-        int st = 0, end = n - 1;
-
-        while(st < end){
+        int st = 0, end =  n - 1;
+        while(st <= end){
             int mid = st + (end - st) / 2;
 
-            // make mid even
-            if(mid % 2 == 1) mid--;
-            if(nums[mid] == nums[mid + 1]){
-                st = mid + 2;
-            } else {
-                end = mid;
+            if(mid == 0 && nums[0] != nums[1]) return nums[mid];
+            if(mid == n - 1 && nums[n - 1] != nums[n - 2]) return nums[mid];
+
+            if(nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1])  return nums[mid];
+
+            if(mid % 2 == 0){ // even condition
+                if(nums[mid -1] == nums[mid]){ // left case 
+                    end = mid -1;
+                }
+                else{  // right case
+                    st = mid + 1;
+                }
+            }
+            else{  // odd condition
+                if(nums[mid - 1] == nums[mid]){  // right case 
+                    st = mid + 1;
+                }
+                else{ // left case
+                    end = mid - 1;
+                }
             }
         }
-        return nums[st];
+        return -1;
 
     }
 };
