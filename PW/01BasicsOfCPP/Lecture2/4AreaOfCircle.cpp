1cead8d7f8f109fd9453b12a1f6de2b15eae8ca3
Admin <admin@example.com>
2026-06-20 19:43:45 +0530

    feat(Lecture2): add 4AreaOfCircle.cpp to calculate area of a circle

---
+++ PW/01BasicsOfCPP/Lecture2/4AreaOfCircle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // float r = 100;
+  // float area = 3.14 * r * r;
+  // cout << area << endl;
+
+  cout << "Enter Radius : ";
+  float r;
+  cin >> r;
+  float area = 3.14 * r * r;
+  cout << "Area of circle is : ";
+  cout << area << endl;
+}
