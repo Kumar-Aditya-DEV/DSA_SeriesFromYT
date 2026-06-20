f2a07277093ce2f33472a02943e48c03ab9c80c8
Admin <admin@example.com>
2026-06-20 19:43:05 +0530

    feat(Lecture2): add 1Basics.cpp demonstrating variables and shorthand operators

---
+++ PW/01BasicsOfCPP/Lecture2/1Basics.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a = 7, b = 4;
+  a = b;
+
+  cout << a << endl;
+  cout << b << endl;
+
+
+
+  int x = 10;
+  x += 7;   // shorthand notation 
+  cout << x << endl;
+
+  x -= 3;
+  cout << x << endl;
+
+  x /= 2;
+  cout << x << endl;
+}
