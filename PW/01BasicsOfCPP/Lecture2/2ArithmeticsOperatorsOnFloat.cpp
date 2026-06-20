6c259c8b72749302e4adac39a378d4c0ade58490
Admin <admin@example.com>
2026-06-20 19:43:31 +0530

    feat(Lecture2): add 2ArithmeticsOperatorsOnFloat.cpp demonstrating float arithmetic operations

---
+++ PW/01BasicsOfCPP/Lecture2/2ArithmeticsOperatorsOnFloat.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float a = 7, b = 4;
+
+  cout << a + b << endl;
+  cout << a - b << endl;
+  cout << a * b << endl;
+  cout << a / b << endl; 
+}
