3daff086e67ea9f2a0465ecf1384d85354f129a4
Admin <admin@example.com>
2026-06-20 19:43:39 +0530

    feat(Lecture2): add 3VariableNamingRules.cpp demonstrating variable naming rules in C++

---
+++ PW/01BasicsOfCPP/Lecture2/3VariableNamingRules.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int xy = 57;
+  cout << xy << endl;
+
+  // int x y = 57
+  // cout << x y << endl;
+
+  // int 1x = 5   //Error
+  // cout << 1x << endl;
+
+
+  //  ! @ # $ % ^ & * ( ) { } [ ] :  ; " " ? / < > , . ` ~ 
+
+
+
+  // Keywords are not allowed  -> if, for,while, dowhile
+
+  int For = 6;
+  cout << For;
+}
