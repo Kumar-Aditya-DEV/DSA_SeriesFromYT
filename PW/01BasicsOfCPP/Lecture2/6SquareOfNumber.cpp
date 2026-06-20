553797e74ca5a067ff78d6c43f2430bfa2d2b66d
Admin <admin@example.com>
2026-06-20 19:43:59 +0530

    feat(Lecture2): add 6SquareOfNumber.cpp to calculate square of a number

---
+++ PW/01BasicsOfCPP/Lecture2/6SquareOfNumber.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  cout << "Enter Your Number : ";
+  float a;
+  cin >> a;
+  float square = a * a;
+  cout << "Square of a number is : ";
+  cout << square << endl;
+}
