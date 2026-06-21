d15e175941a2f21bc04e3f5774f575fb5f5cd0e1
Admin <admin@example.com>
2026-06-21 19:46:29 +0530

    feat(Lecture3): add 9IncrementDecrementOperator.cpp demonstrating increment and decrement operators

---
+++ PW/01BasicsOfCPP/Lecture3/9IncrementDecrementOperator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int x = 10;
+  cout << x << endl;
+  x++;      // x = x + 1
+  ++x;
+  x = x + 1;
+  x += 1;
+  cout << x << endl;
+
+
+  int y = 10;
+  cout << y << endl;
+  y--;      // x = x + 1
+  --y;
+  y = y - 1;
+  y -= 1;
+  cout << y << endl;
+}
