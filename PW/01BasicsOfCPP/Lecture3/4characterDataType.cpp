655c0f6657a9d261e26612c0fe0a15bdf5bee48d
Admin <admin@example.com>
2026-06-21 19:45:28 +0530

    feat(Lecture3): add 4characterDataType.cpp demonstrating character data type

---
+++ PW/01BasicsOfCPP/Lecture3/4characterDataType.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // a b c d ... z
+  // A B C D .... Z
+  // !@#$%^&*()-+=-...
+  // 0 1 2 3 4 5 6 7 8 9
+
+  char x = 'A';
+  cout << x;
+
+  char a = '1';
+  char b = '3';
+}
