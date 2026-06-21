d11f7a55051a76741aeb1b4125ae3e0f0dcac4da
Admin <admin@example.com>
2026-06-21 19:46:11 +0530

    feat(Lecture3): add 7Datatypes.cpp demonstrating division behavior with different data types

---
+++ PW/01BasicsOfCPP/Lecture3/7Datatypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout << 5 + 2 << endl;
+  cout << 5 - 2 << endl;
+  cout << 5 * 2 << endl;
+
+
+  cout << 5 / 2 << endl;           // int
+  cout << 5 / 2.0 << endl;         // float
+  cout << 5.0 / 2 << endl;         // float
+  cout << 5.0 / 2.0 << endl;       // float
+}
