fffa8f0c86dc7f9082c1cb5619de003573ed6ab2
Admin <admin@example.com>
2026-06-21 19:45:18 +0530

    feat(Lecture3): add 3ModulusOperator.cpp demonstrating modulus operator behaviors

---
+++ PW/01BasicsOfCPP/Lecture3/3ModulusOperator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout << 5 % 2;
+  // cout << 5 % 0; //error
+
+  cout << 45 % 241;     // gives 45
+
+  cout << 34 % (-10) << endl;
+
+  cout << (-34) % 10 << endl; 
+
+}
