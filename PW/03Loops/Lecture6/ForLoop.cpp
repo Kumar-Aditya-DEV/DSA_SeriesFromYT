e5b769da473fe2d3436989325e68e1296ed34fb9
Admin <admin@example.com>
2026-07-03 19:31:55 +0530

    feat(Lecture6): add ForLoop.cpp to print message n times using for loop

---
+++ PW/03Loops/Lecture6/ForLoop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+  cout << "Enter the number: ";
+  cin >> n;
+  for (int i = 0; i < n; i++){
+    cout << "Good Morning" << endl;
+  }
+  return 0;
+}
